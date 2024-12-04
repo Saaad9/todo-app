33db109d79681e302b9238d6c2b4097c0282551e
saaad99 <ehdgnstla@naver.com>
2024-12-04 19:11:17 +0900

    [add] localStorage 저장, todo 추가, check 하면 style변경 효과

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
 import './App.css';
-import React, { useState } from'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '할일 1',
-      completed: true
-    },
-    {
-      id: 2,
-      text: '할일 2',
-      completed: false
-    }
-  ]);
+  const [value, setValue] = useState('');
   
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  const getStyle = (completed) => {
+    return {
+        textDecoration: completed ? 'line-through' : 'none', 
+        opacity: completed ? 0.5 : 1  
+    };
+};
+
+  //localStorage 저장
+  useEffect(()=> {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  },[todos]);
+
   const handleClicked = (id) => {
-  //  id에 해당하는 요소의 completed를 변경
-   todos.filter((todo) => {
-      if(todo.id === id) {
+    //  id에 해당하는 요소의 completed를 변경
+    todos.filter((todo) => {
+      if (todo.id === id) {
         todo.completed = !todo.completed;
       }
-   })
-   
-   setTodos([...todos]);
+    });
+
+    setTodos([...todos]);
   }
 
   const handleDelBtn = (id) => {
-    console.log('id : @!@#!@', id);
-    const newTodos =  todos.filter((todo) => todo.id !== id);
+    const newTodos = todos.filter((todo) => todo.id !== id);
 
     setTodos(newTodos);
   }
 
   const handleAllClear = () => {
     setTodos([]);
+  }
+
+  const handleValue = (e) => {
+    setValue(e.target.value);
+  }
+
+  const handleAddTodo = () => {
+    // 만약 아무 값도 입력하지 않았다면 return
+    if (value.trim() === '') return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: value,
+      completed: false
+    }
+    setTodos([...todos, newTodo]);
+    setValue('');
+  }
+
+  const handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
+  };
+
+  const handleFocus = () => {
+    handleAddTodo();
   }
 
 
@@ -46,16 +77,16 @@ function App() {
           <span className='title'>react-todo-app</span>
           <button onClick={() => handleAllClear()} className='clear-btn'>전부 삭제</button>
         </div>
-        
+
         <div className='todo-list'>
 
           {/* todo 추가 */}
-          {todos.map((todo) =>(
-            <div className='todo' key={todo.id}>
-              <input 
-                type='checkbox' 
+          {todos.map((todo) => (
+            <div className='todo' key={todo.id} style={getStyle(todo.completed)}>
+              <input
+                type='checkbox'
                 checked={todo.completed}
-                onChange={() => handleClicked(todo.id) } 
+                onChange={() => handleClicked(todo.id)}
               ></input>
 
               <span>{todo.text}</span>
@@ -64,12 +95,16 @@ function App() {
           ))}
         </div>
 
-        <input 
-          className='input' 
-          type='text' 
+        <input
+          className='input'
+          type='text'
+          value={value}
           placeholder='할일을 입력'
+          onChange={handleValue}
+          onKeyUp={handleKeyUp}
+          onBlur={handleFocus}
         ></input>
-      
+
       </div>
     </div>
   );
